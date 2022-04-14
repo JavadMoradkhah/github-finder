c6433ae908eb5c0c8b6945a34fe7b49f6659a57c
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2022-04-14 18:32:07 +0430

    Clean up actions.

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,28 +1,21 @@
+import axios from 'axios';
+
 const GITHUB_URL = 'https://api.github.com';
 
+const github = axios.create({
+  baseURL: GITHUB_URL,
+});
+
 export const searchUsers = async (text) => {
-  const response = await fetch(`${GITHUB_URL}/search/users?q=${text}`);
-  const { items } = await response.json();
-  return items;
+  const response = await github.get(`/search/users?q=${text}`);
+  return response.data.items;
 };
 
-export const getUser = async (login) => {
-  const response = await fetch(`${GITHUB_URL}/users/${login}`);
-
-  if (response.status === 404) {
-    window.location = '/notfound';
-  } else {
-    const data = await response.json();
-    return data;
-  }
-};
-
-export const getUserRepos = async (login) => {
-  const params = new URLSearchParams({
-    sort: 'created',
-    per_page: 10,
-  });
-  const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`);
-  const data = await response.json();
-  return data;
+// Get user and repos
+export const getUserAndRepos = async (login) => {
+  const [user, repos] = await Promise.all([
+    github.get(`/users/${login}`),
+    github.get(`/users/${login}/repos`),
+  ]);
+  return { user: user.data, repos: repos.data };
 };
